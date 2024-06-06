32e9a3342445c81965694743da9745feceaede71
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-07 01:11:46 +0300

    Binary Trees | Fix Balance Factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * tree_height - Node Height
+ *
+ * @tree: Tree To Be Passed
+ * Return: Height
+ */
+
+int tree_height(const binary_tree_t *tree)
+{
+	int L_Height = 0, R_Height = 0;
+
+	if (!tree)
+		return (0);
+
+	L_Height = 1 + tree_height(tree->left);
+	R_Height = 1 + tree_height(tree->right);
+
+	return (L_Height > R_Height ? L_Height : R_Height);
+}
+
+/**
  * binary_tree_balance - Balance Factor Calc
  * @tree: Tree To Be Passed
  * Return: Balance Factor Num
@@ -8,14 +28,8 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int L_Height = 0, R_Height = 0;
-
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		L_Height = 1 + binary_tree_balance(tree->left);
-	if (tree->right)
-		R_Height = 1 + binary_tree_balance(tree->right);
-	return (L_Height - R_Height);
+	return (tree_height(tree->left) - tree_height(tree->right));
 }
