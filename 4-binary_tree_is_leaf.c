a4dc1393790e5f7b27c941cb513c9bf0b8611737
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-06 21:46:49 +0300

    Binary Tree | Check If A Leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - if a node is a leaf
+ *
+ * @node: Tree To Be Checked
+ * Return: 1 if lead - 0 if Not
+ */
+
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (!node)
+        return (0);
+
+    if (!(node->left) && !(node->right))
+        return (1);
+    return (0);
+}
