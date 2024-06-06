877cd92852b86952dcd92c82f865b9d9e5cd3f06
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-07 01:30:37 +0300

    Binary Trees | Is Perfect Fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 #include "14-binary_tree_balance.c"
-#include "15-binary_tree_is_full.c"
+#include "11-binary_tree_size.c"
 
 /**
  * binary_tree_is_perfect - PerfecT tree
@@ -10,13 +10,17 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int b, f;
+	int height, i;
+	size_t size, max_nodes = 1;
 
 	if (!tree)
 		return (0);
 
-	b = binary_tree_balance(tree);
-	f = binary_tree_is_full(tree);
+	height = tree_height(tree);
+	size = binary_tree_size(tree);
 
-	return (f && !(b));
+	for (i = 0; i < height; i++)
+		max_nodes *= 2;
+
+	return (max_nodes - 1 == size);
 }
