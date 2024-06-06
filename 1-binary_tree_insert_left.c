2011919b05d0701806849a96997ef9b706b52e93
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-06 21:22:03 +0300

    Binary Tree | Insert Node To Left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,18 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Create A new Node
+ * binary_tree_insert_left - Create A new Node ANd insert to left
  *
  * @parent: Parent of new node
  * @value: value stored in the new node
  * Return: New Node or Null if Fails
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *Node = NULL;
 
 	Node = malloc(sizeof(binary_tree_t));
+
+	if (!parent)
+		return (NULL);
 
 	if (!Node)
 		return (NULL);
@@ -21,5 +24,16 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	Node->n = value;
 	Node->left = NULL;
 	Node->right = NULL;
+
+	if (!(parent->left))
+	{
+		parent->left = Node;
+	}
+	else
+	{
+		parent->left->parent = Node;
+		Node->left = parent->left;
+		parent->left = Node;
+	}
 	return (Node);
 }
